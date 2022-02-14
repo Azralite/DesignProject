c6e5b399b36b727a145da6eb447825e1265f8c37
Paul Michel dit Ferrer <paul.mdf@icloud.com>
2022-02-15 00:59:21 +0100

    ajout vote 4 choix

---
+++ vote4.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/vote4Style.css">
+    <title>Voting</title>
+</head>
+
+<body>
+    <div class="mainWrapper">
+        <div class="header">
+            <div class="leftFlag">
+                <p>Country Selection</p>
+                <img src="img/w80/flagFrance.png" alt="flag of france" class="flag">
+                <p>click to vote for different game or country</p>
+            </div>
+            <div class="title">
+
+                <h1>Quizz about <u>France</u></h1>
+            </div>
+            <div class="rightFlags">
+                <p class="from">Where are you from ?</p>
+                <!-- Mixed flag -->
+                <div class="mixedFlag">
+
+                    <img src="img/w80/flagGermany.png" alt="Flag of Germany" class="flag flagFrom">
+                    <img src="img/w80/flagUnitedKingdom.png" alt="Flag of UK" class="flag flagLang">
+                </div>
+                <p class="gameLanguage">Game Language</p>
+            </div>
+        </div>
+
+        <div class="center">
+
+            <div class="answer1">
+                <p>90.000.000</p>
+            </div>
+            <div class="answer2">
+                <p>64.000.000</p>
+            </div>
+            <div class="answer3">
+<p>73.000.000</p>
+            </div>
+            <div class="answer4">
+<p>68.000.000</p>
+            </div>
+
+            <div class="question">
+                <p>How many people live currently in France? <br> Wie viele Menschen leben aktuell in Frankreich?</p>
+            </div>
+        </div>
+
+        <div class="footer">
+            <p>Place your token in one of the four voting areas to select an answer. <br>
+Positioniere deinen Token auf einem der vier Bereiche um eine Antwort auszuwählen.</p>
+        </div>
+    </div>
+
+</body>
+
+</html>
